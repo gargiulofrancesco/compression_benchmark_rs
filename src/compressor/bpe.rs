2b0561b0e8ff875c60cbf0250d3de93adba76fce
Francesco Gargiulo <gargiulo.fr@gmail.com>
2025-05-09 16:23:38 +0000

    simplifying bpe

--- src/compressor/bpe.rs
+++ src/compressor/bpe.rs
@@ -1,14 +1,14 @@
-// BPE v4: explicitly saves token IDs using 2 bytes, with a maximum entry length of 16 bytes in the dictionary.
-
 use super::Compressor;
 use crate::bit_vector::BitVector;
+use std::arch::x86_64::*;
 use std::collections::BinaryHeap;
 use rustc_hash::{FxHashMap, FxHashSet};
 
+const MAX_TOKENS: u16 = 65535;
 const FAST_ACCESS_SIZE: usize = 16;
 
 pub struct BPECompressor {
-    compressed_data: Vec<u16>,                             // Store the compressed data as bytes
+    compressed_data: Vec<u16>,                  // Store the compressed data as bytes
     item_end_positions: Vec<usize>,             // Store the end positions of each compressed item
     dictionary: Vec<u8>,                        // Store the dictionary
     dictionary_end_positions: Vec<u32>,         // Store the end positions of each element in the dictionary
@@ -25,26 +25,133 @@ impl Compressor for BPECompressor {
     }
 
     fn compress(&mut self, data: &[u8], end_positions: &[usize]) {
-        let end_positions_set: FxHashSet<usize> = end_positions.iter().skip(1).copied().collect();
-        let mut next_id: u16 = 256;
+        // Initialize the dictionary with single-byte tokens
+        self.dictionary_end_positions.push(0);
+        for i in 0..256 {
+            let token = vec![i as u8];
+            self.dictionary.extend(&token);
+            self.dictionary_end_positions.push(self.dictionary.len() as u32);
+        }
 
+        // Initialize Token IDs
+        let mut token_ids: Vec<u16> = data.iter().map(|&b| b as u16).collect();
+
+        // The bitvector inidicates with zeroes the positions of merged bytes
         let mut bv = BitVector::with_ones(data.len());
-        let mut token_ids = initialize_token_ids(data, &bv, &mut next_id);
-        let (mut pair_pos, mut max_freq) = initialize_pair_positions(&bv, &token_ids, &end_positions_set);
-        merge(&mut bv, &mut token_ids, &end_positions_set, &mut pair_pos, &mut max_freq, &mut next_id);
+        let end_positions_set: FxHashSet<usize> = end_positions.iter().skip(1).copied().collect();
 
-        let (compressed_strings, compressed_strings_separators, dictionary, dictionary_separators) = remap_by_frequency(data, &bv, &token_ids, end_positions);
-        self.dictionary = dictionary;
-        self.dictionary_end_positions = dictionary_separators;
+        // Initialize pair positions  
+        let mut pair_pos: FxHashMap<(u16, u16), FxHashSet<u32>> = FxHashMap::default();
+        for i in 0..data.len()-1 {
+            if end_positions_set.contains(&(i+1)) {
+                continue; // Avoid pairs that cross end positions
+            }
+            let t1 = token_ids[i];
+            let t2 = token_ids[i+1];
+            pair_pos
+                .entry((t1, t2))
+                .or_insert(FxHashSet::default())
+                .insert(i as u32);
+        }
 
-        let mut start = 0;
-        self.item_end_positions.push(0);
-        for end in compressed_strings_separators {
-            for &token_id in compressed_strings[start..end].iter() {
-                self.compressed_data.push(token_id);
-            } 
+        // Initialize heap tracking the most frequent pairs
+        let mut max_freq: BinaryHeap<(u32, (u16, u16))> = BinaryHeap::new();
+        for (pair, pos_set) in pair_pos.iter() {
+            max_freq.push((pos_set.len() as u32, *pair));
+        }
+
+        // Merge pairs
+        let mut next_id = 256;
+        while next_id<MAX_TOKENS && !max_freq.is_empty(){
+            let (freq, (t1, t2)) = max_freq.pop().unwrap();
+            let current_freq = pair_pos.get(&(t1, t2)).unwrap().len() as u32;
+            if freq != current_freq {
+                continue; // Skip if the frequency is not up-to-date
+            }
+
+            // Get the positions of the pair (t1, t2)
+            let mut positions= pair_pos.remove(&(t1, t2)).unwrap().into_iter().collect::<Vec<u32>>();
+            positions.sort();
+
+            // Add the new token to the dictionary
+            let start = positions[0] as usize;
+            let end = bv.next_one(bv.next_one(start).unwrap()).unwrap_or(bv.len());
+            self.dictionary.extend(&data[start..end]);
+            self.dictionary_end_positions.push(self.dictionary.len() as u32);
+
+            // Store updated pairs to minimize insertions in the max_freq heap
+            let mut updated_pairs: FxHashSet<(u16, u16)> = FxHashSet::default();
+
+            // Update occurrences of (t1, t2)
+            for &position in positions.iter() {
+                // If position was already merged, skip
+                if !bv.get(position as usize).unwrap() {
+                    continue;
+                }
+                
+                let t1_pos = position as usize;
+                let t2_pos = bv.next_one(t1_pos).unwrap();
+                let t0_pos = bv.prev_one(t1_pos);
+                let t3_pos = bv.next_one(t2_pos);
+
+                // Update (t0, t1) and (t0, next_id)  
+                if t0_pos.is_some() && !end_positions_set.contains(&t1_pos) {
+                    let t0 = token_ids[t0_pos.unwrap()];
+                    // Update (t0, t1)
+                    if (t0, t1) != (t1, t2) {
+                        pair_pos.get_mut(&(t0, t1)).unwrap().remove(&(t0_pos.unwrap() as u32));
+                        updated_pairs.insert((t0, t1));
+                    }
+                    // Update (t0, next_id)
+                    pair_pos
+                            .entry((t0, next_id))
+                            .or_insert(FxHashSet::default())
+                            .insert(t0_pos.unwrap() as u32);
+                    updated_pairs.insert((t0, next_id));
+                }
+
+                // Update (t2, t3) and (next_id, t3)
+                if t3_pos.is_some() && !end_positions_set.contains(&t3_pos.unwrap()){
+                    let t3 = token_ids[t3_pos.unwrap()];
+                    // Update (t2, t3)
+                    if (t2, t3) != (t1, t2) {
+                        pair_pos.get_mut(&(t2, t3)).unwrap().remove(&(t2_pos as u32));
+                        updated_pairs.insert((t2, t3));
+                    }
+                    // Update (next_id, t3)
+                    pair_pos
+                            .entry((next_id, t3))
+                            .or_insert(FxHashSet::default())
+                            .insert(t1_pos as u32);
+                    updated_pairs.insert((next_id, t3));
+                }
+    
+                // set t2_pos to 0 to merge t1 and t2
+                bv.set(t2_pos as usize, false);
+    
+                // Update the token_ids
+                token_ids[t1_pos] = next_id;
+            }
+    
+            // Update the max_freq heap with updated pairs
+            for &pair in updated_pairs.iter() {
+                let freq = pair_pos.get(&pair).unwrap().len() as u32;
+                max_freq.push((freq, pair));
+            }
+    
+            next_id += 1;
+        }
+
+        // Store the compressed data
+        let mut i = 0;
+        for &end_position in end_positions.iter() {
+            while i < end_position {
+                if bv.get(i).unwrap() {
+                    self.compressed_data.push(token_ids[i]);
+                }
+                i += 1;
+            }
             self.item_end_positions.push(self.compressed_data.len());
-            start = end;
         }
     }
 
@@ -107,301 +214,10 @@ impl Compressor for BPECompressor {
     }
 
     fn space_used_bytes(&self) -> usize {
-        (2 * self.compressed_data.len()) + self.dictionary.len() + (self.dictionary_end_positions.len() * 4)
+        (self.compressed_data.len() * std::mem::size_of::<u16>()) + self.dictionary.len() + (self.dictionary_end_positions.len() * std::mem::size_of::<u32>())
     }
 
     fn name(&self) -> &str {
         "BPE"
     }
-}
-
-/// Initializes token IDs for the given data based on token boundaries defined by a `BitVector`
-fn initialize_token_ids(data: &[u8], bv: &BitVector, next_id: &mut u16) -> Vec<u16> {
-    let mut token_ids: Vec<u16> = vec![0; data.len()];
-    let mut map: FxHashMap<Vec<u8>, u16> = FxHashMap::default();
-
-    let mut iter = bv.ones(0);
-    let mut current_pos = iter.next().unwrap();
-    let mut next_pos_opt = iter.next();
-
-    while let Some(next_pos) = next_pos_opt {
-        let slice = &data[current_pos..next_pos];
-        let token_id = get_or_insert_token(&mut map, slice, next_id);
-        token_ids[current_pos] = token_id;
-
-        current_pos = next_pos;
-        next_pos_opt = iter.next();
-    }
-
-    // Add the last token
-    let slice = &data[current_pos..];
-    let token_id = get_or_insert_token(&mut map, slice, next_id);
-    token_ids[current_pos] = token_id;
-
-    token_ids.shrink_to_fit();
-
-    token_ids
-}
-
-/// Retrieves an existing token ID for a token or inserts a new one if it's not in the map.
-#[inline(always)]
-fn get_or_insert_token(map: &mut FxHashMap<Vec<u8>, u16>, slice: &[u8], next_id: &mut u16) -> u16 {
-    if slice.len() == 1 {
-        slice[0] as u16
-    }
-    else if let Some(&token_id) = map.get(slice) {
-        token_id
-    }
-    else {
-        map.insert(slice.to_vec(), *next_id);
-        *next_id += 1;
-        *next_id - 1
-    }
-}
-
-/// Initializes pair positions based on the tokenization provided by a `BitVector`
-fn initialize_pair_positions(bv: &BitVector, token_ids: &[u16], end_positions_set: &FxHashSet<usize>) -> (FxHashMap<(u16, u16), FxHashSet<u32>>, BinaryHeap<(u32, (u16, u16))>) {
-    let mut pair_pos: FxHashMap<(u16, u16), FxHashSet<u32>> = FxHashMap::default();
-    let mut max_freq: BinaryHeap<(u32, (u16, u16))> = BinaryHeap::new();
-    
-    let mut iter = bv.ones(0);
-    let mut current_pos = iter.next().unwrap();
-    let mut next_pos_opt = iter.next();
-
-    while let Some(next_pos) = next_pos_opt {
-        // Skip pairs (a, b) where b is the first token of a string to avoid merging strings
-        if !end_positions_set.contains(&next_pos) {
-            let t1 = token_ids[current_pos];
-            let t2 = token_ids[next_pos];
-            pair_pos
-                .entry((t1, t2))
-                .or_insert(FxHashSet::default())
-                .insert(current_pos as u32);
-        }
-
-        current_pos = next_pos;
-        next_pos_opt = iter.next();
-    }
-
-    for (pair, pos_set) in pair_pos.iter() {
-        max_freq.push((pos_set.len() as u32, *pair));
-    }
-
-    (pair_pos, max_freq)
-}
-
-fn merge (
-    bv: &mut BitVector, 
-    token_ids: &mut [u16], 
-    end_positions_set: &FxHashSet<usize>,
-    pair_pos: &mut FxHashMap<(u16, u16), FxHashSet<u32>>, 
-    max_freq: &mut BinaryHeap<(u32, (u16, u16))>,
-    next_id: &mut u16,
-) {
-    while *next_id < u16::MAX {
-        // Store updated pairs to minimize insertions in the max_freq heap
-        let mut updated_pairs: FxHashSet<(u16, u16)> = FxHashSet::default();
-
-        // Get the pair with the maximum frequency
-        let (freq, (t1, t2)) = loop {
-            let (freq, (t1, t2)) = max_freq.pop().unwrap();
-            let current_freq = pair_pos.get(&(t1, t2)).unwrap().len() as u32;
-
-            // Check if the frequency is up-to-date
-            if freq == current_freq {
-                let t1_pos = *pair_pos.get(&(t1, t2)).unwrap().iter().next().unwrap() as usize;
-                let t2_pos = bv.next_one(t1_pos).unwrap();
-                let t3_pos = bv.next_one(t2_pos).unwrap_or(bv.len());
-                let length = t3_pos - t1_pos;
-                if length > 16 {
-                    continue;
-                }
-
-                break (freq, (t1, t2));  // Exit loop with valid pair
-            }
-        };
-
-        if freq < 10 {
-            break;
-        }
-
-        // Get the positions of the pair (t1, t2)
-        let mut positions= pair_pos.remove(&(t1, t2)).unwrap().into_iter().collect::<Vec<u32>>();
-        positions.sort();
-
-        // Update occurrences of (t1, t2)
-        for &position in positions.iter() {
-            // If position was already merged, skip
-            if bv.get(position as usize).unwrap() == false {
-                continue;
-            }
-            
-            let t1_pos = position as usize;
-            let t2_pos = bv.next_one(t1_pos).unwrap();
-            let t0_pos = bv.prev_one(t1_pos);
-            let t3_pos = bv.next_one(t2_pos);
-
-            // Get the previous token (if it exists)
-            let t0 = t0_pos.map(|t0_pos| token_ids[t0_pos]);
-
-            // Get the next token (if it exists)
-            let t3 = t3_pos.map(|t3_pos| token_ids[t3_pos]);
-
-            // Update (t0, t1) and (t0, next_id)             
-            if let Some(t0) = t0 {
-                // If t1 is a separator, don't update the pair (t0, t1)
-                if !end_positions_set.contains(&t1_pos) {
-                    updated_pairs.insert((t0, t1));
-                    updated_pairs.insert((t0, *next_id));
-                    // Update the pair (t0, t1)
-                    if let Some(pos_set) = pair_pos.get_mut(&(t0, t1)) {
-                        pos_set.remove(&(t0_pos.unwrap() as u32));
-                    }
-                    // Update the pair (t0, next_id)
-                    pair_pos
-                        .entry((t0, *next_id))
-                        .or_insert(FxHashSet::default())
-                        .insert(t0_pos.unwrap() as u32);
-                }
-            }
-
-            // Update (t2, t3) and (next_id, t3)
-            if let Some(t3) = t3 {
-                if !end_positions_set.contains(&t3_pos.unwrap()) {
-                    updated_pairs.insert((t2, t3));
-                    updated_pairs.insert((*next_id, t3));
-                    // Update the pair (t2, t3)
-                    if let Some(pos_set) = pair_pos.get_mut(&(t2, t3)) {
-                        pos_set.remove(&(t2_pos as u32));
-                    }
-                    // Update the pair (next_id, t3)
-                    pair_pos
-                        .entry((*next_id, t3))
-                        .or_insert(FxHashSet::default())
-                        .insert(t1_pos as u32);
-                }
-            }
-
-            // set t2_pos to 0 to merge t1 and t2
-            bv.set(t2_pos as usize, false);
-
-            // Update the token_ids
-            token_ids[t1_pos] = *next_id;
-        }
-
-        // Update the max_freq heap with updated pairs
-        for &(ti, tj) in updated_pairs.iter() {
-            if (ti, tj) != (t1, t2) {
-                let freq = pair_pos.get(&(ti, tj)).unwrap().len() as u32;
-                max_freq.push((freq, (ti, tj)));
-            }
-        }
-
-        *next_id += 1;
-    }
-}
-
-fn remap_by_frequency(data: &[u8], bv: &BitVector, token_ids: &[u16], end_positions: &[usize]) -> (Vec<u16>, Vec<usize>, Vec<u8>, Vec<u32>) {
-    let mut compressed_strings = get_new_token_ids(bv, token_ids);
-    let mut compressed_strings_separators = get_new_strings_separators(bv, end_positions);
-
-    // get the frequency of each token_id
-    let mut frequency_map: FxHashMap<u16, u32> = FxHashMap::default();
-    for &token_id in compressed_strings.iter() {
-        *frequency_map.entry(token_id).or_insert(0) += 1;
-    }
-
-    // sort token ids by frequency
-    let mut freq_vec: Vec<_> = frequency_map.iter().collect();
-    freq_vec.sort_by(|a, b| b.1.cmp(a.1));
-
-    // populate the dictionary
-    let dictionary_map = get_dictionary_map(data, bv, token_ids);
-    let mut dictionary = Vec::new();
-    let mut dictionary_separators  = vec![0u32];
-    for &(token_id, _) in freq_vec.iter() {
-        let bytes = dictionary_map.get(&token_id).unwrap();
-        dictionary.extend_from_slice(bytes);
-        dictionary_separators.push(dictionary.len() as u32);
-    }
-
-    // create a map from token_id to frequency rank
-    let mut rank_map: FxHashMap<u16, u16> = FxHashMap::default();
-    for (rank, (&token_id, _)) in freq_vec.iter().enumerate() {
-        rank_map.insert(token_id, rank as u16);
-    }
-
-    // remap the token_ids by frequency rank
-    for token_id in compressed_strings.iter_mut() {
-        *token_id = *rank_map.get(token_id).unwrap();
-    }
-
-    compressed_strings.shrink_to_fit();
-    compressed_strings_separators.shrink_to_fit();
-    dictionary.shrink_to_fit();
-    dictionary_separators.shrink_to_fit();
-
-    (compressed_strings, compressed_strings_separators, dictionary, dictionary_separators)
-}
-
-fn get_dictionary_map(data: &[u8], bv: &BitVector, token_ids: &[u16]) -> FxHashMap<u16, Vec<u8>> {
-    let mut dictionary_map: FxHashMap<u16, Vec<u8>> = FxHashMap::default();
-    
-    let mut iter = bv.ones(0);
-    let mut current_pos = iter.next().unwrap();
-    let mut next_pos_opt = iter.next();
-
-    while let Some(next_pos) = next_pos_opt {
-        let slice = &data[current_pos..next_pos];
-        let token_id = token_ids[current_pos];
-        dictionary_map.entry(token_id).or_insert(slice.to_vec());
-
-        current_pos = next_pos;
-        next_pos_opt = iter.next();
-    }
-
-    let slice = &data[current_pos..];
-    let token_id = token_ids[current_pos];
-    dictionary_map.entry(token_id).or_insert(slice.to_vec());
-
-    dictionary_map
-}
-
-fn get_new_token_ids(bv: &BitVector, token_ids: &[u16]) -> Vec<u16> {
-    let mut new_token_ids: Vec<u16> = Vec::new();
-
-    for pos in bv.ones(0) {
-        new_token_ids.push(token_ids[pos]);
-    }
-
-    new_token_ids
-}
-
-fn get_new_strings_separators(bv: &BitVector, end_positions: &[usize]) -> Vec<usize> {
-    let mut new_strings_separators = Vec::new();
-    let mut current_size = 0;
-
-    for window in end_positions.windows(2) {
-        let start = window[0];
-        let end = window[1];
-        
-        // Handle empty strings
-        if start == end {
-            new_strings_separators.push(current_size);
-            continue;
-        }
-
-        for pos in bv.ones(start) {
-            if pos == end {
-                new_strings_separators.push(current_size);
-                break;
-            }
-            current_size += 1;
-        }
-    }
-
-    new_strings_separators.push(current_size);
-    new_strings_separators.shrink_to_fit();
-
-    new_strings_separators
 }
